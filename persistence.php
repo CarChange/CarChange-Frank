05ba6f775867d26a56b8711cca155a6cf1a3ce0d
Lukas Fernando Menezes <Lukas Fernando Menezes>
2017-03-03 15:18:08 -0300

    ajustes bestas

--- persistence.php
+++ persistence.php
@@ -1,8 +1,10 @@
 <?php
+// Server Data
 $servername = "localhost";
 $username = "u427946632_luke";
 $password = "fallout";
 $dbname = "u427946632_teste";
+//Form Data
 $nome = $_POST["nome"];
 $email = $_POST["email"];
 $telefone = $_POST["telefone"];
@@ -17,8 +19,9 @@ if ($conn->connect_error) {
 $sql = "INSERT INTO usuarios (nome, email, telefone) VALUES ('{$nome}','{$email}','{$telefone}')";
 
 if ($conn->query($sql) === TRUE) {
+    //Msg
     echo "<script>
-        alert('Dados enviados com sucesso!');
+        alert('Cadastro realizado com sucesso!');
         window.location.href='index.html';
     </script>";
 } else {
