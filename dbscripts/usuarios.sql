1bfb4209b523464a58ce3eb8914d795313fe63cc
Lukas Fernando Menezes <Lukas Fernando Menezes>
2017-03-03 10:50:59 -0300

    Mudei mesmo

--- dbscripts/usuarios.sql
+++ dbscripts/usuarios.sql
@@ -1,10 +1,11 @@
-CREATE DATABASE 'usarios_CarChange';
-USE 'usarios_CarChange';
 
-CREATE TABLE 'usuarios' (
-	'id' int(11) NOT NULL AUTO_INCREMENT,
-	'cpf' varchar(11),
-	'nome' varchar(80) NOT NULL,
-	'email' varchar(255) NOT NULL,
-	'telefone' varchar(15) NOT NULL
-) ENGINE = InnoDB AUTO_INCREMENT = 11 DEFAULT CHARSET = latin1;
+CREATE TABLE `usuarios` (
+	`id` int(11) NOT NULL AUTO_INCREMENT KEY,
+	`cpf` varchar(11),
+	`nome` varchar(80) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`telefone` varchar(15) NOT NULL
+) 
+
+INSERT INTO `players` (`cpf`,`nome`,`email`,`telefone`) VALUES 
+  ('1234567','Lukas','kukafernando@hotmail.com','981557716'),('6666666','Christofer','christofer666@diabo.com','981234561');
