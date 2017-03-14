a2c5f8aaac9c0bce9e50a464d78ce6d3f4e1aa34
ChrisCWerner <ccwerner96@gmail.com>
2017-03-14 19:16:32 -0300

    form quase completo

---
+++ cadastro.js
@@ -0,0 +1,8 @@
+function validateEmail() {
+    var x = document.forms["formA"]["email"].value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        return false;
+    }
+}
