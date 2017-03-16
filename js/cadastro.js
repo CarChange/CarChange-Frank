0b727922f4ca5f843a6bcd6890b861b611fce2a7
ChrisCWerner <ccwerner96@gmail.com>
2017-03-16 17:50:14 -0300

    dá pull daniel

--- js/cadastro.js
+++ js/cadastro.js
@@ -13,3 +13,19 @@ function showHideConjuge(elem) {
         console.log("opção 'casado' não selecionado.");
     }
 }
+
+function showHideEmprego(elem) {
+    var sectionComercial = document.getElementById("endcom");
+    var navbarComercial = document.getElementById("navbar-endcom");
+    
+    if(elem.value == "desempregado"){
+        sectionComercial.classList.add("hidden");
+        navbarComercial.classList.add("hidden");
+        console.log("opção 'desempregado' selecionado.");
+    }
+    else {
+        sectionComercial.classList.remove("hidden");
+        navbarComercial.classList.remove("hidden");
+        console.log("opção 'desempregado' não selecionado.");
+    }
+}
