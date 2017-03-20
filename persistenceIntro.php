719653f67178bab23b77bb134cf73349fe9c1e48
Lukas Fernando Menezes <Lukas Fernando Menezes>
2017-03-20 17:11:04 -0300

    Schrodinger

--- persistenceIntro.php
+++ persistenceIntro.php
@@ -8,6 +8,7 @@ $dbname = "u427946632_carc";
 $nome          = $_POST["nome"];
 $email         = $_POST["email"];
 $telefone      = $_POST["telefone"];
+$DDD$DDD           = $_POST["DDD"];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -16,7 +17,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO `prospeccao`(`nome`, `telefone`, `email`) VALUES ('{$nome}','{$telefone}','{$email}')";
+$sql = "INSERT INTO `prospeccao`(`nome`, `telefone`, `email`) VALUES ('{$nome}','{$DDD.$telefone}','{$email}')";
 
 if ($conn->query($sql) === TRUE) {
     //Msg
